0eb4c4524e01b0cd7319a3f3e7968fccc0391346
Sarthak Khandelwal (SAKH) <sakh@saxobank.com>
2017-11-06 11:07:42 +0530

    added login folder

---
+++ login/login.js
@@ -0,0 +1,7 @@
+//document.getElementById('button_login').addEventListener('click',submitInfo);
+function submitInfo()
+{
+    var  username = document.getElementById('field_userid').value;
+    var password = document.getElementById('field_password').value;
+
+}
